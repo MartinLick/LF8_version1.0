27e1d4ceb1e9ee3393c206d9660b25a4a2e76285
MartinLick <martin.lick@deutschebahn.com>
2022-04-27 11:01:02 +0200

    Habe die App nochmal neu erstellt

---
+++ app/src/main/java/com/example/lf8_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lf8_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
